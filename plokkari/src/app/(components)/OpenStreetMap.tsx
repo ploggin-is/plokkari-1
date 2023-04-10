8f32175094710841d21faf2e6c61058667653fb0
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2023-04-10 16:19:04 +0000

    Get users location
    Now, the initial location on the map is the user location if we can access it. Otherwise, we set the centre of the map to Reykjavík University.
    
    Note that when there is a little delay when getting the location.

--- plokkari/src/app/(components)/OpenStreetMap.tsx
+++ plokkari/src/app/(components)/OpenStreetMap.tsx
@@ -1,18 +1,66 @@
 "use client"
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
-import L from 'leaflet';
+import React, { useEffect, useState} from "react";
 import "leaflet/dist/leaflet.css";
 
-const OpenStreetMap = ({ center, zoom }) => {
-  return (
-    <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} style={{ width: "100%", height: "100vh", margin: '0'}}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
-        contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-  );
+
+
+const OpenStreetMap = ({ zoom }) => {
+  var [mapCenter, setMapCenter] = useState(null);
+
+  const options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000
+  };
+
+  useEffect(() => {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        // Save the location
+        setMapCenter({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+
+      },
+      (error: GeolocationPositionError) => {
+        console.log("Error getting location")
+
+        // In case of an error, show Reykjavík University
+        setMapCenter({
+          latitude: 64.124025,
+          longitude: -21.925479
+        });
+      },
+      options
+    );
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+    // In case of an error, show Reykjavík University
+    setMapCenter({
+      latitude: 64.124025,
+      longitude: -21.925479
+    });
+  }
+}, []);
+
+// Wait while geting location
+if (!mapCenter) {
+  // ToDo: Make nicer loading-screen
+  return <p>Loading...</p>;
+}
+const { latitude, longitude } = mapCenter;
+return (
+  <MapContainer center={[latitude, longitude]} zoom={zoom} scrollWheelZoom={true} style={{ width: "100%", height: "100vh", margin: '0'}}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
+      contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  </MapContainer>
+);
 };
 
 export default OpenStreetMap;
--- plokkari/src/app/page.tsx
+++ plokkari/src/app/page.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <OpenStreetMap center={[51.505, -0.09]} zoom={13} />
+      <OpenStreetMap zoom={13} />
     </main>
   )
 }

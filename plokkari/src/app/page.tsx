4b479be2f6027f39cf1c28aa69070be3364581a7
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2023-04-11 10:14:13 +0000

    Created the clan/Dirty button

--- plokkari/src/app/page.tsx
+++ plokkari/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import OpenStreetMap from './(components)/OpenStreetMap'
-
+import CleanButton from './(components)/CleanButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <OpenStreetMap center={[51.505, -0.09]} zoom={13} />
+      <CleanButton />
     </main>
   )
 }

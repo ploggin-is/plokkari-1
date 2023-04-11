fedb4508cb44eb327bd2ba86f46a35ac964694c0
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2023-04-11 13:06:50 +0000

    Created the verify selection popup

--- plokkari/src/app/(components)/OpenStreetMap.tsx
+++ plokkari/src/app/(components)/OpenStreetMap.tsx
@@ -1,9 +1,13 @@
 "use client"
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
+import React, { useState } from "react";
 import "leaflet/dist/leaflet.css";
+import VerifySelection from './(VerifySelection)/VerifySelection'
+
 
 const OpenStreetMap = ({ center, zoom }) => {
+  const [showVerifySelection, setShowVerifySelection] = useState(false);
   return (
     <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} style={{ width: "100%", height: "100vh", margin: '0'}}>
       <TileLayer
@@ -11,6 +15,7 @@ const OpenStreetMap = ({ center, zoom }) => {
         contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <VerifySelection showPopup= {showVerifySelection} setShowPopup={setShowVerifySelection} center={[51.505, -0.09]} zoom={13}/>
     </MapContainer>
   );
 };

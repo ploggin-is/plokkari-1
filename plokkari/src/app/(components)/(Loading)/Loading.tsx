c4dd5e049d895dd640f913255f573f76de144ac8
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2023-04-19 21:50:47 +0000

    move from react-lottie to lottie-web
    and remove duplicates due to merge conflict

--- plokkari/src/app/(components)/(Loading)/Loading.tsx
+++ plokkari/src/app/(components)/(Loading)/Loading.tsx
@@ -1,32 +1,33 @@
-"use client"
-import Lottie from "react-lottie";
-import animationData from '../../../../public/58737-simple-green-loading-animation'
+"use client";
+import { useEffect, useRef } from "react";
+import lottie from "lottie-web";
+import animationData from "../../../../public/58737-simple-green-loading-animation.json";
 
-const loading = () => {
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
+const Loading = () => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    if (container.current) {
+      lottie.loadAnimation({
+        container: container.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData,
+      });
     }
-    };
-    
-    return (
-      <div
-        style={{
-          background: 'white',
-          width: '100%',
-          height: '100vh',
-        }}
-      >
-      <Lottie 
-        options={defaultOptions}
-        height={200}
-        width={200}
-      />
-      </div>
-    );
+  }, []);
+
+  return (
+    <div
+      ref={container}
+      style={{
+        background: "white",
+        width: "100%",
+        height: "100vh",
+      }}
+    ></div>
+  );
 };
 
-export default loading;
+export default Loading;
